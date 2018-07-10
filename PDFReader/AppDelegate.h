4c671dd807b4b8f913de07a95d509f037e949d8e
fangshufeng <fangsh@dianrong.com>
2018-07-10 10:03:33 +0800

    Initial Commit

---
+++ PDFReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PDFReader
+//
+//  Created by fangshufeng on 2018/7/10.
+//  Copyright © 2018 fangshufeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
